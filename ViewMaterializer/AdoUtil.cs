04aa96b403ef95bd693d2daaf2ad4cb2d8a2e73a
Adam O'Neil <adamosoftware@gmail.com>
2018-12-04 14:35:13 -0500

    first commit

---
+++ ViewMaterializer/AdoUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ViewMaterializer
+{
+	public static class AdoUtil
+	{
+		public static DataTable QueryTable(this SqlConnection connection, string selectQuery, Action<SqlCommand> setParameters = null)
+		{
+			using (var cmd = new SqlCommand(selectQuery, connection))
+			{
+				setParameters?.Invoke(cmd);
+				using (var adapter = new SqlDataAdapter(cmd))
+				{
+					DataTable result = new DataTable();
+					adapter.Fill(result);
+					return result;
+				}
+			}
+		}
+
+		public static T QueryValue<T>(this SqlConnection connection, string query, Action<SqlCommand> setParameters = null)
+		{
+			var table = QueryTable(connection, query, setParameters);
+			return table.Rows[0].Field<T>(table.Columns[0]);
+		}
+
+		public static void Execute(this SqlConnection connection, string commandText, CommandType commandType = CommandType.Text, Action<SqlCommand> setParameters = null)
+		{
+			using (var cmd = new SqlCommand(commandText, connection))
+			{
+				cmd.CommandType = commandType;
+				setParameters?.Invoke(cmd);
+				cmd.ExecuteNonQuery();
+			}
+		}
+	}
+}
---
+++ ViewMaterializer/ViewMaterializer.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ViewMaterializer
+{
+	public abstract class ViewMaterializer
+	{
+		public ViewMaterializer(string sourceView, string targetTable)
+		{
+			SourceView = sourceView;
+			TargetTable = targetTable;
+		}
+
+		public string SourceView { get; }
+		public string TargetTable { get; }
+
+		public void Execute(SqlConnection connection)
+		{
+			long currentVersion = GetCurrentVersion(connection);
+			long latestSyncVersion = GetLatestSyncVersion(connection);
+
+			// do everything
+
+			SetLatestSyncVersion(connection, currentVersion);
+		}
+
+		/// <summary>
+		/// Saves the latest sync version so next time we we know which version to start with
+		/// </summary>
+		protected abstract void SetLatestSyncVersion(SqlConnection connection, long currentVersion);
+
+		/// <summary>
+		/// Where do we start are sync from?
+		/// </summary>
+		protected abstract long GetLatestSyncVersion(SqlConnection connection);
+
+		/// <summary>
+		/// Return value of this will be used the next time we get changes
+		/// </summary>
+		private long GetCurrentVersion(SqlConnection connection)
+		{
+			return connection.QueryValue<long>("SELECT CHANGE_TRACKING_CURRENT_VERSION()");
+		}
+	}
+}
